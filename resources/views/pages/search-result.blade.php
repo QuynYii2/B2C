d2bb0a06ca329529281e62720dcb56889a3b37a3
Quy Nguyen <quy.nguyen@Quys-MacBook-Pro.local>
2023-06-17 11:32:37 +0700

    test git cpanel

--- resources/views/pages/search-result.blade.php
+++ resources/views/pages/search-result.blade.php
@@ -63,7 +63,7 @@
 @section('content')
     <div class="container-fluid">
         <h2>Kết quả tìm kiếm sản phẩm: {{$nameProduct}}</h2>
-
+        @dd('test')
     @if(isset($data['items']) && count($data['items']['item']) > 0)
             <div class="row">
                 @foreach($data['items']['item'] as $item)
